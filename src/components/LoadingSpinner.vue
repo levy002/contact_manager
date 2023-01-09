b73904b2df36a8414d542c409512b483c1007420
“levy-DELL” <levy.ukwi002@gmail.com>
2023-01-09 17:05:57 +0200

    Add loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <img src="../assets/Color-Loading-2.gif" alt="Spinner" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LoadingSpinner",
+};
+</script>
+
+<style></style>
