28fa147773af2d6522cad1848bb589ec8af701b3
“levy-DELL” <levy.ukwi002@gmail.com>
2023-01-09 17:09:58 +0200

    Render all components

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+
+export default {
+  name: "App",
+  components: { NavBar },
+};
+</script>
+
+<style></style>
